88bd4a6576268c82d76b2fc3ea7b716de8c83c6d
Alexey Selivanov <lazypacman@gmail.com>
2026-05-31 22:28:47 +0400

    Add GitHub Pages base path support

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const repositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
+const isGitHubPagesBuild = process.env.GITHUB_ACTIONS === "true";
+const isUserOrOrgPagesSite = repositoryName.endsWith(".github.io");
+
 export default defineConfig({
+  base: isGitHubPagesBuild ? (isUserOrOrgPagesSite ? "/" : `/${repositoryName}/`) : "/",
   plugins: [react()]
 });
